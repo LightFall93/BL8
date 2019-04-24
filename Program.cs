23f2d165694264531c3a790dc678745fc1b3015f
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-04-24 23:22:20 +0300

    1

--- Program.cs
+++ Program.cs
@@ -8,7 +8,32 @@ namespace BL8_P1_P2
 {
     class Program
     {
-       
+        static void Main(string[] args)
+        {
+            /*Rectangle f1;
+            f1.a_side = 10;
+            f1.b_side = 5;
+            f1.coor = 5;
+            f1.x = 0;
+            f1.y = 0;*/
+
+            List<Rectangle> parts = new List<Rectangle>();
+
+
+        }
+
+        struct Rectangle
+        {
+            public int b_side;
+            public int a_side;
+            public int coor;
+            public int x;
+            public int y;
+
+
+        }
+
+
     }
 
 }
