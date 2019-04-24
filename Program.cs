233a51fd0b85f03a7c0fcfa5e6d66416b7895b4a
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-04-24 23:20:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL8_P1_P2
+{
+    class Program
+    {
+       
+    }
+
+}
